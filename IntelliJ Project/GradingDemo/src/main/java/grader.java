88eb92894648e38474a616d02b2ef76aed4bf8e1
Kenneth Vo <kennethvo2003@gmail.com>
2025-10-30 09:35:35 -0500

    latest version with no errors and optimized code

--- IntelliJ Project/GradingDemo/src/main/java/grader.java
+++ IntelliJ Project/GradingDemo/src/main/java/grader.java
@@ -20,18 +20,14 @@ public class grader {
                 continue;
             }
 
-            if (grade >= 90) {
-                System.out.println("You got an A");
-            } else if (grade >= 80) {
-                System.out.println("You got a B");
-            } else if (grade >= 70) {
-                System.out.println("You got a C");
-            } else if (grade >= 60) {
-                System.out.println("You got a D");
-            } else {
-                System.out.println("You got an F");
+            if (grade >= 90) { System.out.println("You got an A");
+            } else if (grade >= 80) {  System.out.println("You got a B");
+            } else if (grade >= 70) {  System.out.println("You got a C");
+            } else if (grade >= 60) { System.out.println("You got a D");
+            } else { System.out.println("You got an F");
             }
 
+            input.close();
             break;
         }
     }
